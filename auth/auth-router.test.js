abebfb092771fe09f899f8ab829f07a6ca88b5bf
dominique maack <dominiquemaack@gmail.com>
2019-11-15 10:56:10 -0700

    2 test written for /login + working -- MVP

--- auth/auth-router.test.js
+++ auth/auth-router.test.js
@@ -1,6 +1,8 @@
 const request = require('supertest');
 const server = require('../api/server');
 const db = require('../database/dbConfig');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 describe('Auth Router', () => {
     beforeEach(async () => {
@@ -21,6 +23,30 @@ describe('Auth Router', () => {
                 .send({ username: 'maack', password: 'password'})
 
             expect(res.body.user).toBe('maack')
+            // try hashed password test at end
+        })
+    })
+    describe('POST /login endpoint', () => {
+        it('should return a 200 http status code and successfully login a new user', () => {
+            const userBody = { username: 'mango', password: 'puppypassword' } 
+
+            request(server).post('/api/auth/register').send(userBody)
+                .then(res => {
+                    return request(server).post('/api/auth/login').send(userBody)
+                        .then(res => {
+                            expect(res.type).toMatch(/json/i)
+                            expect(res.status).toBe(200)
+                        })
+                })
+        })
+        it('should return an object wiht a Welcome message', async () => {
+            const userBody = { username: 'Thomas', password: 'password' }
+
+            await request(server).post('/api/auth/register').send(userBody)
+
+            const res = await request(server).post('/api/auth/login').send(userBody)
+
+            expect(res.body.message).toBe('Welcome Thomas')
         })
     })
 })
