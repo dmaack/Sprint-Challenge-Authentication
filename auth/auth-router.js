5bae03b6b1ce5d95817b0b0a767b3be6e4fa047b
dominique maack <dominiquemaack@gmail.com>
2019-11-15 09:58:41 -0700

    /login endpoint working

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -28,7 +28,25 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  const { username, password } = req.body;
+
+  Users.findBy({ username })
+  .first()
+  .then(user => {
+    if(user && bcrypt.compareSync(password, user.password)) {
+      const token = generateToken(user.username)
+
+      res.status(200).json({
+        message: `Welcome ${user.username}`,
+        token
+      })
+    } else {
+      res.status(401).json({ message: 'Invalid Credentials' })
+    }
+  })
+  .catch(err => {
+    res.status(500).json(err)
+  })
 });
 
 module.exports = router;
