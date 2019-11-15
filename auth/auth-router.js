8af5c3b34877a8dc06f6b233e5d2dd83bab9d7ce
dominique maack <dominiquemaack@gmail.com>
2019-11-15 09:53:14 -0700

    fixed dbConfig and register endpoint wokring

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,30 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
+const Users = require('./auth-model');
+
+const generateToken = require('./generateToken');
+const authenticate = require('./authenticate-middleware');
 
 router.post('/register', (req, res) => {
-  // implement registration
+  const user = req.body;
+
+  if(!user.username || !user.password) {
+    res.status(401).json({ error: 'Bad request'})
+  } else  {
+    const hash = bcrypt.hashSync(user.password, 10)
+    user.password = hash;
+
+    const token = generateToken(user)
+
+    Users.add(user)
+      .then(saved => {
+        res.status(201).json({ user: user.username, token })
+      })
+      .catch(err => {
+        res.status(500).json(err)
+      })
+  }
 });
 
 router.post('/login', (req, res) => {
